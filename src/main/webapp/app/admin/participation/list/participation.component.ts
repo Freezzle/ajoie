3b0fd674af2e6e4f57aaf9e57c230306f8a1e311
dylan <dylan_claude@hotmail.com>
2024-09-01 12:20:25 +0200

    Improvment

--- src/main/webapp/app/admin/participation/list/participation.component.ts
+++ src/main/webapp/app/admin/participation/list/participation.component.ts
@@ -1,123 +1,123 @@
-import {Component, inject, NgZone, OnInit} from '@angular/core';
-import {ActivatedRoute, Router, RouterModule} from '@angular/router';
-import {combineLatest, filter, Observable, Subscription, tap} from 'rxjs';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { combineLatest, filter, Observable, tap } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import SharedModule from 'app/shared/shared.module';
-import {SortByDirective, SortDirective, SortService} from 'app/shared/sort';
-import {DurationPipe, FormatMediumDatePipe, FormatMediumDatetimePipe} from 'app/shared/date';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ITEM_DELETED_EVENT} from 'app/config/navigation.constants';
-import {IParticipation} from '../participation.model';
-import {EntityArrayResponseType, ParticipationService} from '../service/participation.service';
-import {ParticipationDeleteDialogComponent} from '../delete/participation-delete-dialog.component';
+import { SortByDirective, SortDirective } from 'app/shared/sort';
+import { DurationPipe, FormatMediumDatePipe, FormatMediumDatetimePipe } from 'app/shared/date';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ITEM_DELETED_EVENT } from 'app/config/navigation.constants';
+import { IParticipation } from '../participation.model';
+import { EntityArrayResponseType, ParticipationService } from '../service/participation.service';
+import { ParticipationDeleteDialogComponent } from '../delete/participation-delete-dialog.component';
 import ColorStatusPipe from '../../../shared/pipe/color-status.pipe';
 import StatusPipe from '../../../shared/pipe/status.pipe';
 import CheckBoolPipe from '../../../shared/pipe/check-boolean.pipe';
 import ColorBoolPipe from '../../../shared/pipe/color-boolean.pipe';
 import FilterComponent from '../../../shared/filter/filter.component';
-import {Status} from '../../enumerations/status.model';
+import { Status } from '../../enumerations/status.model';
 
 @Component({
-               standalone: true,
-               selector: 'jhi-participation',
-               templateUrl: './participation.component.html',
-               imports: [
-                   RouterModule,
-                   FormsModule,
-                   SharedModule,
-                   SortDirective,
-                   SortByDirective,
-                   DurationPipe,
-                   FormatMediumDatetimePipe,
-                   FormatMediumDatePipe,
-                   ColorStatusPipe,
-                   StatusPipe,
-                   CheckBoolPipe,
-                   ColorBoolPipe,
-                   FilterComponent,
-                   ReactiveFormsModule,
-               ],
-           })
+  standalone: true,
+  selector: 'jhi-participation',
+  templateUrl: './participation.component.html',
+  imports: [
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    SortDirective,
+    SortByDirective,
+    DurationPipe,
+    FormatMediumDatetimePipe,
+    FormatMediumDatePipe,
+    ColorStatusPipe,
+    StatusPipe,
+    CheckBoolPipe,
+    ColorBoolPipe,
+    FilterComponent,
+    ReactiveFormsModule,
+  ],
+})
 export class ParticipationComponent implements OnInit {
-    subscription: Subscription | null = null;
-    participations?: IParticipation[];
-    isLoading = false;
-    statusValues = Object.keys(Status);
-    params: any;
+  participations?: IParticipation[];
+  isLoading = false;
+  statusValues = Object.keys(Status);
+  params: any;
+  filters: FormGroup<any> = new FormGroup<any>({
+    fullName: new FormControl(),
+    status: new FormControl(),
+  });
 
-    public router = inject(Router);
-    protected participationService = inject(ParticipationService);
-    protected activatedRoute = inject(ActivatedRoute);
-    protected sortService = inject(SortService);
-    protected modalService = inject(NgbModal);
-    protected ngZone = inject(NgZone);
+  public router = inject(Router);
+  protected participationService = inject(ParticipationService);
+  protected activatedRoute = inject(ActivatedRoute);
+  protected modalService = inject(NgbModal);
 
-    ngOnInit(): void {
-        combineLatest([this.activatedRoute.paramMap, this.activatedRoute.data]).subscribe(([params, data]) => {
-            this.params = params;
-            if (!this.participations || this.participations.length === 0) {
-                this.load();
-            }
-        });
+  ngOnInit(): void {
+    combineLatest([this.activatedRoute.paramMap, this.activatedRoute.data]).subscribe(([params, data]) => {
+      this.params = params;
+      if (!this.participations || this.participations.length === 0) {
+        this.load();
+      }
+    });
+  }
+
+  delete(participation: IParticipation): void {
+    const modalRef = this.modalService.open(ParticipationDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.participation = participation;
+    // unsubscribe not needed because closed completes on modal close
+    modalRef.closed
+      .pipe(
+        filter(reason => reason === ITEM_DELETED_EVENT),
+        tap(() => this.load()),
+      )
+      .subscribe();
+  }
+
+  actionFilter(): void {
+    this.load();
+
+    const fullNameFilter = this.filters.get('fullName')?.value;
+    if (fullNameFilter && fullNameFilter.length > 0) {
+      this.participations = this.participations?.filter(participation =>
+        participation.exhibitor?.fullName?.toLocaleLowerCase().includes(this.filters.get('fullName')?.value),
+      );
     }
-
-    delete(participation: IParticipation): void {
-        const modalRef = this.modalService.open(ParticipationDeleteDialogComponent, {size: 'lg', backdrop: 'static'});
-        modalRef.componentInstance.participation = participation;
-        // unsubscribe not needed because closed completes on modal close
-        modalRef.closed
-                .pipe(
-                    filter(reason => reason === ITEM_DELETED_EVENT),
-                    tap(() => this.load()),
-                )
-                .subscribe();
+    const statusFilter = this.filters.get('status')?.value;
+    if (statusFilter && statusFilter.length > 0) {
+      this.participations = this.participations?.filter(participation => participation.status?.includes(this.filters.get('status')?.value));
     }
+  }
 
-    load(): void {
-        this.queryBackend().subscribe({
-                                          next: (res: EntityArrayResponseType) => {
-                                              this.onResponseSuccess(res);
-                                          },
-                                      });
-    }
+  load(): void {
+    this.queryBackend().subscribe({
+      next: (res: EntityArrayResponseType) => {
+        this.onResponseSuccess(res);
+      },
+    });
+  }
 
-    previousState(): void {
-        window.history.back();
-    }
+  refresh(): void {
+    this.filters.reset();
+    this.load();
+  }
 
-    filteringExhibitor(event: any): void {
-        if (event.target.value) {
-            this.participations = this.participations?.filter(participation =>
-                                                                  participation.exhibitor?.fullName?.toLocaleLowerCase()
-                                                                               .includes(
-                                                                                   event.target.value.toLocaleLowerCase()),
-            );
-        } else {
-            this.load();
-        }
-    }
+  previousState(): void {
+    window.history.back();
+  }
 
-    filteringStatus(event: any): void {
-        if (event.target.value) {
-            this.participations = this.participations?.filter(
-                participation => participation.status?.includes(event.target.value));
-        } else {
-            this.load();
-        }
-    }
+  protected onResponseSuccess(response: EntityArrayResponseType): void {
+    const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
+    this.participations = dataFromBody;
+  }
 
-    protected onResponseSuccess(response: EntityArrayResponseType): void {
-        const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
-        this.participations = dataFromBody;
-    }
+  protected fillComponentAttributesFromResponseBody(data: IParticipation[] | null): IParticipation[] {
+    return data ?? [];
+  }
 
-    protected fillComponentAttributesFromResponseBody(data: IParticipation[] | null): IParticipation[] {
-        return data ?? [];
-    }
-
-    protected queryBackend(): Observable<EntityArrayResponseType> {
-        this.isLoading = true;
-        return this.participationService.query(this.params.get('idSalon')).pipe(tap(() => (this.isLoading = false)));
-    }
+  protected queryBackend(): Observable<EntityArrayResponseType> {
+    this.isLoading = true;
+    return this.participationService.query(this.params.get('idSalon')).pipe(tap(() => (this.isLoading = false)));
+  }
 }
