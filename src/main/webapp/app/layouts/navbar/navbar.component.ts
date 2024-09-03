8a29fefbe906ad5a42f1f19943a8e6a77360f5f7
dylan <dylan_claude@hotmail.com>
2024-09-03 18:42:54 +0200

    Multi filters

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,73 +1,71 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
-import {Router, RouterModule} from '@angular/router';
-import {TranslateService} from '@ngx-translate/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
-import {StateStorageService} from 'app/core/auth/state-storage.service';
+import { StateStorageService } from 'app/core/auth/state-storage.service';
 import SharedModule from 'app/shared/shared.module';
 import HasAnyAuthorityDirective from 'app/shared/auth/has-any-authority.directive';
-import {VERSION} from 'app/app.constants';
-import {LANGUAGES} from 'app/config/language.constants';
-import {AccountService} from 'app/core/auth/account.service';
-import {LoginService} from 'app/login/login.service';
-import {ProfileService} from 'app/layouts/profiles/profile.service';
+import { VERSION } from 'app/app.constants';
+import { LANGUAGES } from 'app/config/language.constants';
+import { AccountService } from 'app/core/auth/account.service';
+import { LoginService } from 'app/login/login.service';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
 import ActiveMenuDirective from './active-menu.directive';
-import NavbarItem from './navbar-item.model';
 
 @Component({
-               standalone: true,
-               selector: 'jhi-navbar',
-               templateUrl: './navbar.component.html',
-               styleUrl: './navbar.component.scss',
-               imports: [RouterModule, SharedModule, HasAnyAuthorityDirective, ActiveMenuDirective],
-           })
+  standalone: true,
+  selector: 'jhi-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  imports: [RouterModule, SharedModule, HasAnyAuthorityDirective, ActiveMenuDirective],
+})
 export default class NavbarComponent implements OnInit {
-    inProduction?: boolean;
-    isNavbarCollapsed = signal(true);
-    languages = LANGUAGES;
-    openAPIEnabled?: boolean;
-    version = '';
-    account = inject(AccountService).trackCurrentAccount();
-    entitiesNavbarItems: NavbarItem[] = [];
+  inProduction?: boolean;
+  isNavbarCollapsed = signal(true);
+  languages = LANGUAGES;
+  openAPIEnabled?: boolean;
+  version = '';
+  account = inject(AccountService).trackCurrentAccount();
 
-    private loginService = inject(LoginService);
-    private translateService = inject(TranslateService);
-    private stateStorageService = inject(StateStorageService);
-    private profileService = inject(ProfileService);
-    private router = inject(Router);
+  private loginService = inject(LoginService);
+  private translateService = inject(TranslateService);
+  private stateStorageService = inject(StateStorageService);
+  private profileService = inject(ProfileService);
+  private router = inject(Router);
 
-    constructor() {
-        if (VERSION) {
-            this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
-        }
+  constructor() {
+    if (VERSION) {
+      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
     }
+  }
 
-    ngOnInit(): void {
-        this.profileService.getProfileInfo().subscribe(profileInfo => {
-            this.inProduction = profileInfo.inProduction;
-            this.openAPIEnabled = profileInfo.openAPIEnabled;
-        });
-    }
+  ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.openAPIEnabled = profileInfo.openAPIEnabled;
+    });
+  }
 
-    changeLanguage(languageKey: string): void {
-        this.stateStorageService.storeLocale(languageKey);
-        this.translateService.use(languageKey);
-    }
+  changeLanguage(languageKey: string): void {
+    this.stateStorageService.storeLocale(languageKey);
+    this.translateService.use(languageKey);
+  }
 
-    collapseNavbar(): void {
-        this.isNavbarCollapsed.set(true);
-    }
+  collapseNavbar(): void {
+    this.isNavbarCollapsed.set(true);
+  }
 
-    login(): void {
-        this.router.navigate(['/login']);
-    }
+  login(): void {
+    this.router.navigate(['/login']);
+  }
 
-    logout(): void {
-        this.collapseNavbar();
-        this.loginService.logout();
-        this.router.navigate(['']);
-    }
+  logout(): void {
+    this.collapseNavbar();
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
 
-    toggleNavbar(): void {
-        this.isNavbarCollapsed.update(isNavbarCollapsed => !isNavbarCollapsed);
-    }
+  toggleNavbar(): void {
+    this.isNavbarCollapsed.update(isNavbarCollapsed => !isNavbarCollapsed);
+  }
 }
