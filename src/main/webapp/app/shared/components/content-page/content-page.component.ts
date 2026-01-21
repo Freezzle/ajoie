0cef78be3fe85ae68d9c3a9aba83bd36f465f6b6
dylan <dylan.claude.work@gmail.com>
2026-01-21 07:02:53 +0100

    feat: topbar

--- src/main/webapp/app/shared/components/content-page/content-page.component.ts
+++ src/main/webapp/app/shared/components/content-page/content-page.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
 import {Menubar} from 'primeng/menubar';
+import {CardComponent} from '../card/card.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
                selector: 'app-content-page',
                imports: [
-                   Menubar
+                   Menubar,
+                   CardComponent,
+                   TranslateModule
                ],
                templateUrl: './content-page.component.html',
                styleUrl: './content-page.component.scss'
